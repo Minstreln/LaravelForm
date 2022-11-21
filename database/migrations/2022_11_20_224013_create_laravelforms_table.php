b0f335bef6fa4fbf710355a9798dcd550ae45fd8
Minstreln <minstreln@gmail.com>
2022-11-21 16:38:00 +0100

    Added Backend

---
+++ database/migrations/2022_11_20_224013_create_laravelforms_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('laravelforms', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email');
+            $table->string('password');
+            $table->string('country');
+            $table->string('gender');
+            $table->string('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('laravelforms');
+    }
+};
